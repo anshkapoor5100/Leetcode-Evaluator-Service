b49f74c1ddc099e92a18b493efdc9d9d461bb32c
ansh kapoor <anshkapoor5100@gmail.com>
2025-10-02 19:38:43 +0530

    basic bullmq reddis setup

---
+++ src/config/redisConfig.ts
@@ -0,0 +1,10 @@
+import {Redis} from "ioredis";
+import ServerConfig from "./serverConfig.js";
+const redisConfig = {
+  port: ServerConfig.REDIS_PORT || 6379,
+  host: ServerConfig.REDIS_HOST || '127.0.0.1',
+};
+
+const redisConnection = new Redis(redisConfig);
+
+export default redisConnection;
